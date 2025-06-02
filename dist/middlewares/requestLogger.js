333c1dfae2bbd03d7ab672e32007607a3cd49df7
vaalley <vaaleyou@gmail.com>
2025-06-02 13:34:34 +0200

    Stop tracking dist directory

--- dist/middlewares/requestLogger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.requestLogger = requestLogger;
-function requestLogger(request, response, next) {
-    console.log(`[${request.method}] - ${request.path}`);
-    next();
-}
